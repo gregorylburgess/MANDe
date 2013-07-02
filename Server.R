546d105d1cef977dc76afb6b7d48b2ee6a636b74
Gregory Burgess <gregory.l.burgess@gmail.com>
2013-07-01 21:51:27 -1000

    new

--- Server.R
+++ Server.R
@@ -2,13 +2,12 @@ library('Rook')
 source('Apps.R')
 
 rook <- Rhttpd$new()
-
-rook$start(listen="0.0.0.0", port=as.numeric(Sys.getenv("PORT")))
-#rook$start('127.0.0.1', 8000)
 rook$add(index,'index')
 rook$add(helloWorld,'helloWorld')
 rook$add(summary,'summary')
 rook$add(rookTestApp,'rookTestApp')
+# rook$start('127.0.0.1', 8000)
+rook$start(listen="0.0.0.0", port=as.numeric(Sys.getenv("PORT")))
 
 # check if we should stop
 #while(TRUE) {
