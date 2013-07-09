c62ec50741abdaf3a49540d69060090aef552d39
Gregory Burgess <gregory.l.burgess@gmail.com>
2013-07-08 16:29:01 -1000

    Test.

--- demo.R
+++ demo.R
@@ -1,7 +1,12 @@
 require('Rook')
 rook <- Rhttpd$new()
-rook$start(listen="0.0.0.0", port=as.numeric(Sys.getenv("PORT")))
 
-while(T) {
-	Sys.sleep(10000)
+helloWorld <- function(env){
+	req <- Rook::Request$new(env)
+	res <- Rook::Response$new()
+	res$write("HELLO WORLD")
+	res$finish()
 }
+
+rook$add(helloWorld,'helloWorld')
+rook$start(listen="0.0.0.0", port=as.numeric(Sys.getenv("PORT")))
