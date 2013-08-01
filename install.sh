6b3ecda1b1bbd86bffab9e6f90e7fc765a882366
Gregory Burgess <gregory.l.burgess@gmail.com>
2013-07-31 21:17:54 -1000

    Editing the install script.

--- install.sh
+++ install.sh
@@ -4,7 +4,5 @@ sudo apt-get install git
 sudo apt-get install libnetcdf-dev
 git clone https://github.com/gregorylburgess/acoustic-deploy.git
 cd acoustic-deploy
-cd ncdf
-export NETCDF_INCLUDE=/usr/include/
-export NETCDF_LIB=/user/lib/
+sudo R CMD INSTALL --configure-args="-with-netcdf-include=/usr/include/ -with-netcdf-lib=/usr/lib/" ncdf_1.6.6.tar.gz
 Rscript install.R
