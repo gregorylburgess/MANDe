08d8bfad6f5f4e5b7ffefe599df3ff68e6c04dbf
Gregory Burgess <gregory.l.burgess@gmail.com>
2013-08-05 01:07:30 -1000

    Fixed a js bug where script was submitting default values.

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ sudo add-apt-repository ppa:marutter/rdev
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install r-base-dev
-sudo apt-get update
 sudo apt-get install git
 sudo apt-get install libgdal-dev
-sudo apt-get install libprojectm-qt-dev
+sudo apt-get install libproj-dev
 sudo apt-get install libnetcdf-dev
+sudo apt-get update
 cd acoustic-deploy
 sudo R CMD INSTALL --configure-args="-with-netcdf-include=/usr/include/ -with-netcdf-lib=/usr/lib/" ncdf_1.6.6.tar.gz
 sudo Rscript install.R
